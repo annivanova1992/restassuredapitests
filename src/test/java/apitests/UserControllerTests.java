12e21b78ae534324b67b4e480c9ec207b41fdc19
Anna.Dobrovidova <Anna.Dobrovidova@nordigy.ru>
2022-11-06 17:03:42 +0400

    Add several tests

--- src/test/java/apitests/UserControllerTests.java
+++ src/test/java/apitests/UserControllerTests.java
@@ -4,14 +4,42 @@ import static io.restassured.RestAssured.*;
 
 import org.junit.Test;
 
+import io.restassured.response.ResponseBody;
+
+import com.google.gson.*;
+
+import static org.hamcrest.Matchers.equalTo;
+
 public class UserControllerTests {
 	@Test
 	public void getUserTestReturns404IfNotFound() {
 		given()
-		.pathParam("uid", "59")
+			.pathParam("uid", "59")
 		.when()
 			.get("http://checker.jettycloud.com:8080/api/public/user/info/{uid}")
 		.then()
 			.assertThat().statusCode(404);
+	}
+	
+	@Test
+	public void getUserTestReturnsError() {
+		given()
+			.pathParam("uid", "59")
+		.when()
+			.get("http://checker.jettycloud.com:8080/api/public/user/info/{uid}")
+		.then()
+			.assertThat().statusCode(404);
+	}
+	
+	@Test
+	public void postUserStart() {
+		int uid = 59; 
+		given()
+			.pathParam("uid", uid)
+		.when()
+			.post("http://checker.jettycloud.com:8080/api/public/user/start/{uid}")
+		.then()
+			.assertThat()
+			.body("message", equalTo("User with uid " + uid + " not found"));
 	}
 }
